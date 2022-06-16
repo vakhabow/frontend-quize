fd2d5e5879ff7778f017af9f18816aa9280e330e
Galahad <100938093+Ya-Aladdin@users.noreply.github.com>
2022-06-17 00:47:22 +0300

    сделал футер

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import React from "react";
+import styles from "./Footer.module.css";
+import { BsWhatsapp } from 'react-icons/bs';
+import { AiOutlineInstagram, AiOutlineYoutube, AiOutlineTwitter } from 'react-icons/ai';
+import { GrFacebook } from 'react-icons/gr';
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <footer className={styles.footer}>
+      <div>
+        <h1 className={styles.footer_logo_name}>Quizee</h1>
+        <p>Квизи — интерактивные тесты онлайн, бесплатно. © 2022 «Квизи»</p>
+        <div className={styles.icons}>
+          <BsWhatsapp />
+          <AiOutlineInstagram />
+          <AiOutlineYoutube />
+          <AiOutlineTwitter />
+          <GrFacebook />
+        </div>
+      </div>
+      <div className={styles.lists_wrap}>
+      <ul className={styles.list}>
+        <li>О нас</li>
+        <li>Как это работает</li>
+        <li>Отзывы</li>
+        <li>Продукты</li>
+        <li>Вопросы и ответы</li>
+        <li>Идеи викторин</li>
+        <li>Партнерская программа</li>
+      </ul>
+      <ul className={styles.list}>
+        <li>Наши партнеры</li>
+        <li>Блог</li>
+        <li>Поддержка</li>
+        <li>Условия использования</li>
+      </ul>
+      </div>
+    </footer>
   );
 };
 
